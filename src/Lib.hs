415708c6b4740b8a7862762719d2e8f9bdc15b5e
cp2n23 <cp2n23@soton.ac.uk>
2025-03-12 11:29:09 +0000

    added CSV module for read/write

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,18 @@
 module Lib (
-    introMessage
+    introMessage,
+    runQuery
   ) where
+
+import CSV (CSV, readCSVFile, writeCSVFile, sortCSV)
 
 introMessage :: String
 introMessage =
-  "Congratulations, your project builds. Now try writing it."
+  "CSV Query Language Interpreter"
+
+-- | Run a query on CSV files and write the output
+-- This is just a placeholder for now
+runQuery :: String -> [FilePath] -> FilePath -> IO (Either String ())
+runQuery _query _inputFiles outputFile = do
+  -- Placeholder implementation:
+  -- Just outputs an empty CSV file
+  writeCSVFile outputFile []
